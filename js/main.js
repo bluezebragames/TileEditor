b6d62cf6e53cb189b57e75b524d3a55d3c941c68
bluezebragames <bluezebragames@gmail.com>
2017-07-26 22:14:06 -0600

    Still missing some grid lines

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ window.onload = function() {
 	var gridsize_string = prompt("What size do you want you want?  Enter your input in the form CxR, where C is the number of columns and R is the number of rows.");
 	gridsize = gridsize_string.split("x").map(Number);
 	canvas = document.getElementById("canvas");
-	canvas.width = gridsize[0]*16+gridsize[0];
-	canvas.height = gridsize[1]*16+gridsize[1];
+	canvas.width = gridsize[0]*16+gridsize[0]+1;
+	canvas.height = gridsize[1]*16+gridsize[1]+1;
 	ctx = canvas.getContext("2d");
 	ctx.beginPath();
 	ctx.lineWidth = 1;
-	for(var i = 0; i<gridsize[0]; ++i) {
+	for(var i = 0; i<gridsize[0]+1; ++i) {
 		ctx.moveTo(i*16+i, 0);
 		ctx.lineTo(i*16+i, canvas.height);
 	}
-	for(var i = 0; i<gridsize[1]; ++i) {
+	for(var i = 0; i<gridsize[1]+1; ++i) {
 		ctx.moveTo(0, i*16+i);
 		ctx.lineTo(canvas.height, i*16+i);
 	}
