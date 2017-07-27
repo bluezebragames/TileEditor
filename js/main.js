f09be1ef31c19c3ff0c7d24642bf40963681f5df
bluezebragames <bluezebragames@gmail.com>
2017-07-26 21:34:58 -0600

    Trying some prompt things.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+var canvas, ctx, gridsize;
+
+
 window.onload = function() {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
+	var gridsize_string = prompt("What size do you want you want?");
+	gridsize = gridsize_string.split("x");
+	canvas = document.getElementById("canvas");
+	canvas.width = gridsize[0];
+	canvas.height = gridsize[1];
+	console.log(gridsize);
+	ctx = canvas.getContext("2d");
 	ctx.rect(20,20,150,100);
 	ctx.stroke();
 };
