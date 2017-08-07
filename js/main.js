08e1fc4b1de3e2f9f787f7313ba6cbc77b900d26
bluezebragames <bluezebragames@gmail.com>
2017-08-06 20:13:52 -0600

    Can place tiles.  cool.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
-var canvas, ctx, gridsize;
+var canvas, ctx, gridSize;
+var tileSize = 32;
+var borderThickness = 1;
+var currentTile = 0;
+
+var images = [];
+var loadImages = function() {
+	images.push(new Image);
+	images[0].src = "static/dirt.png"
+	images[0].draw = drawImageActualSize;
+	images.push(new Image);
+	images[1].src = "static/grass.png"
+	images[1].draw = drawImageActualSize;
+
+	function drawImageActualSize(x, y) {
+		ctx.drawImage(this, parseInt(x/(tileSize+borderThickness))*(tileSize+borderThickness), parseInt(y/(tileSize+borderThickness))*(tileSize+borderThickness));
+	}
+}
 
 
 window.onload = function() {
-	var gridsize_string = prompt("What size do you want you want?  Enter your input in the form CxR, where C is the number of columns and R is the number of rows.");
-	gridsize = gridsize_string.split("x").map(Number);
+	loadImages();
+	var gridSize_string = prompt("What size do you want you want?  Enter your input in the form CxR, where C is the number of columns and R is the number of rows.");
+	gridSize = gridSize_string.split("x").map(Number);
 	canvas = document.getElementById("canvas");
-	canvas.width = gridsize[0]*16+gridsize[0]+1;
-	canvas.height = gridsize[1]*16+gridsize[1]+1;
+	canvas.addEventListener('click', function(event) {
+		var Left = canvas.offsetLeft;
+		var Top = canvas.offsetTop;
+		var x = event.pageX - Left;
+        var y = event.pageY - Top;
+		images[currentTile].draw(x, y);
+	});
+	canvas.width = gridSize[0]*tileSize+gridSize[0]+1;
+	canvas.height = gridSize[1]*tileSize+gridSize[1]+1;
 	ctx = canvas.getContext("2d");
 	ctx.beginPath();
-	ctx.lineWidth = 1;
-	for(var i = 0; i<gridsize[0]+1; ++i) {
-		ctx.moveTo(i*16+i, 0);
-		ctx.lineTo(i*16+i, canvas.height);
+	ctx.lineWidth = borderThickness;
+	for(var i = 0; i<gridSize[0]+1; ++i) {
+		ctx.moveTo(i*tileSize+i, 0);
+		ctx.lineTo(i*tileSize+i, canvas.height);
 	}
-	for(var i = 0; i<gridsize[1]+1; ++i) {
-		ctx.moveTo(0, i*16+i);
-		ctx.lineTo(canvas.height, i*16+i);
+	for(var i = 0; i<gridSize[1]+1; ++i) {
+		ctx.moveTo(0, i*tileSize+i);
+		ctx.lineTo(canvas.height, i*tileSize+i);
 	}
 	ctx.stroke();
 };
